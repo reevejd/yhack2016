e6f75a1546bd8f79317601259195b18a4e4e562c
James Reeve <reevejamesd@gmail.com>
2016-11-12 05:59:22 -0500

    map updates in real time to match database

--- app/src/main/java/com/example/litmus/litmus/MapActivity.java
+++ app/src/main/java/com/example/litmus/litmus/MapActivity.java
@@ -1,25 +1,93 @@
 package com.example.litmus.litmus;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import java.util.Map;
 
+import com.esri.android.map.GraphicsLayer;
 import com.esri.android.map.MapView;
+import com.esri.android.map.popup.PopupContainer;
+import com.esri.core.geometry.GeometryEngine;
+import com.esri.core.geometry.Point;
+import com.esri.core.map.Graphic;
+import com.esri.core.symbol.SimpleMarkerSymbol;
+import com.esri.core.geometry.SpatialReference;
+import com.esri.core.symbol.TextSymbol;
+import com.esri.core.symbol.TextSymbol.*;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.view.View;
+
+import java.util.HashMap;
 
 public class MapActivity extends Activity {
 
+    FirebaseDatabase database;
+    DatabaseReference locations;
+
+
 
     MapView mMapView;
+    GraphicsLayer graphicsLayer;
+    SpatialReference webSR;
+    PopupContainer popupContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        webSR = SpatialReference.create(3857);
+
 
         // after the content of this activity is set
         // the map can be accessed from the layout
         mMapView = (MapView)findViewById(R.id.map);
+        graphicsLayer = new GraphicsLayer();
+        mMapView.addLayer(graphicsLayer);
+        popupContainer = new PopupContainer(mMapView);
+
+        database = FirebaseDatabase.getInstance();
+        locations = database.getReference("Locations");
+
+        locations.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.d("firebase listener", "value is " + snapshot);
+                // every time this is modified, redraw the map
+                //graphicsLayer = new GraphicsLayer(); // clear the graphics layer
+                graphicsLayer.removeAll();
+                for (DataSnapshot location : snapshot.getChildren()) {
+                    String thisLocationName = (String) location.child("name").getValue();
+                    Double thisLocationLatitude = (Double) location.child("latitude").getValue();
+                    Double thisLocationLongitude = (Double) location.child("longitude").getValue();
+                    int attendees = 0;
+                    for (DataSnapshot person : location.child("people").getChildren()) {
+                        attendees++;
+                        // get some data from these people later, to get who's present, demographics, etc.
+                    }
+                    createPointOfInterest(thisLocationLatitude, thisLocationLongitude, thisLocationName, attendees);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w("firebase listener error",error.toException());
+            }
+        });
+
+
+        //createPointOfInterest(41.3163, -72.9223, "Placeholder", 5);
+
     }
 
 
@@ -41,6 +109,23 @@ public class MapActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
 
+    protected void createPointOfInterest(Double latitude, Double longitude, String description, int population) {
+        Log.d("POI", "entered point of interest creator");
+        Point pnt = GeometryEngine.project(longitude, latitude, webSR);
+
+        SimpleMarkerSymbol sms = new SimpleMarkerSymbol(Color.RED, 40, SimpleMarkerSymbol.STYLE.CIRCLE);
+        Graphic graphic = new Graphic(pnt, sms);
+        graphicsLayer.addGraphic(graphic);
+
+        TextSymbol textSymbol = new TextSymbol(30, Integer.toString(population), Color.WHITE, HorizontalAlignment.CENTER, VerticalAlignment.MIDDLE);
+        Graphic textGraphic = new Graphic(pnt, textSymbol);
+        graphicsLayer.addGraphic(textGraphic);
+
+
+
+    }
+
+
+}
 
